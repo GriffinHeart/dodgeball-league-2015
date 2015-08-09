fabe8c81e4abbdc2bdcbdaf3c3eb3eca9aa654bf
Hugo Almeida <squall.griffinheart@gmail.com>
2015-08-09 16:47:29 +0900

    updating urls

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,7 +2,7 @@ class MainController {
   constructor($http) {
     'ngInject';
 
-    $http.get('/assets/data.json').
+    $http.get('assets/data.json').
       then((response)=> {
         this.data = response.data;
       });
